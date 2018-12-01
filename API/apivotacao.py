3b8ed41bdc1c9a2beaae25f71ca363a36972a9cd
Gabriel Pereira <gabriellr1908@gmail.com>
2018-12-01 15:04:35 -0200

    Atualização da API votos.

--- API/apivotacao.py
+++ API/apivotacao.py
@@ -1,10 +1,27 @@
 #coding: utf-8
 
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 import json
-from flask import Flask
 
+UPLOAD_FOLDER = './imagens'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def inicio():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        return json.dumps({'erro': 'Utilize o metodo GET para acessar essa páginas.'})
+
 
 @app.route('/candidato/<voto>', methods=['GET'])
 def candidato(voto):
@@ -19,8 +36,46 @@ def candidato(voto):
 
 @app.route('/voto/<voto>', methods=['GET'])
 def vota(voto):
-        return json.dumps({'mensagem': 'Confirmado'})
+    return json.dumps({'mensagem': 'Confirmado'})
 
+# A partir do cadastro de um formulário, a API obtém os valores
+# escolhidos pelo usuário, e a imagem do candidato.
+# Retorna um json no formato abaixo
+# {
+#  'nome': 'Bulbassauro',
+#  'numeroCandidato': 17,
+#  'partido': 'PJL',
+#  'imagem': 'Bulbassauro.png'
+# }
+@app.route('/cadastrar', methods=['GET','POST'])
+def cadastrarCandidato():
+    if request.method == 'POST':
+        # Obtem o nome do candidato para salvar a imagem
+        nomeCandidato = request.form['nomeCandidato']
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # Transforma o nome da imagem obtida para o nome do candidato
+            nomeImagem = nomeCandidato + '.' + filename.split('.')[1]
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], nomeImagem))
+
+        candidato = {}
+        candidato['nome'] = nomeCandidato
+        candidato['numeroCandidato'] = int(request.form['numCandidato'])
+        candidato['partido'] = request.form['partido']
+        candidato['imagem'] = nomeImagem
+        return json.dumps(candidato)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.1.142")
+    app.run(debug=True)
