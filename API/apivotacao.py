f083bd72c75ccfb1f18b1e8797791d7020624a80
José de Melo <josedemelo.if@gmail.com>
2018-12-04 21:40:08 -0200

    [UP] Página inicial

--- API/apivotacao.py
+++ API/apivotacao.py
@@ -4,31 +4,34 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import json
-import eleicao
+##import eleicao
 
 
-NAME_APP = 'NOME-APLICACAO'
+NAME_APP = 'Eleições TSI'
 
-UPLOAD_FOLDER = './imagens'
+UPLOAD_FOLDER = './static/imagens'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+candidatos = {}
+
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def inicio():
     if request.method == 'GET':
-        return render_template('index.html', nome=NAME_APP, cadastrarCandidato = True)
+        return render_template('index.html', nome=NAME_APP)
     else:
         return json.dumps({'erro': 'Utilize o metodo GET para acessar essa páginas.'})
 
-
+'''
 @app.route('/candidato/<int:numero>', methods=['GET'])
 def candidato(numero):
     dados = None
@@ -44,6 +47,8 @@ def candidato(numero):
     return json.dumps(dados)
 
 
+
+
 @app.route('/voto/<int:numero>', methods=['GET'])
 def vota(numero):
 
@@ -54,7 +59,7 @@ def vota(numero):
         print(e)
 
     return json.dumps({'mensagem': 'Confirmado'})
-
+'''
 
 
 # A partir do cadastro de um formulário, a API obtém os valores
@@ -63,7 +68,7 @@ def vota(numero):
 # {
 #  'nome': 'Bulbassauro',
 #  'numeroCandidato': 17,
-#  'partido': 'PJL',
+#  'partido': 'PSL',
 #  'imagem': 'Bulbassauro.png'
 # }
 @app.route('/cadastrar', methods=['GET','POST'])
@@ -89,15 +94,17 @@ def cadastrarCandidato():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], nomeImagem))
 
 
+
+        nCandidato = int(request.form['numCandidato'])
         candidato = {}
         candidato['nome'] = nomeCandidato
-        candidato['numeroCandidato'] = int(request.form['numCandidato'])
+        candidato['numeroCandidato'] = nCandidato
         candidato['partido'] = request.form['partido']
-        candidato['imagem'] = nomeImagem
-        return json.dumps(candidato)
+        candidato['nome_imagem'] = nomeImagem
 
-
-    return render_template('index.html', nome=NAME_APP)
+        candidatos[nCandidato] = candidato
+        print(candidatos)
+        return render_template('index.html', nome=NAME_APP, candidatoCadastrado = candidato)
 
 
 @app.route('/apurar', methods=['GET','POST'])
@@ -105,4 +112,4 @@ def apurar():
     return json.dumps(eleicao.apurar_votacao())
 
 if __name__ == '__main__':
-    app.run(debug=True , host='10.3.1.21')
+    app.run(debug=True)
